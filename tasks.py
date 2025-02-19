717f6f50e5738bf56ada6bd390672cee3c8122c5
RESEGO MOTLHASI <remotlhjhb024@student.wethinkcode.co.za>
2025-02-19 19:01:41 +0200

    Added basic functionality to view tasks and view tasks

--- tasks.py
+++ tasks.py
@@ -3,7 +3,7 @@ user_tasks = input("Enter a task (q to quit): ")
 
 while True:
     if user_tasks.lower() == "q":
-        print("Bye bye!")
+        print("You stopped adding tasks")
         break
    
     else:
@@ -12,4 +12,24 @@ while True:
         else:
             tasks.append(user_tasks)
         user_tasks = input("Enter a task (q to quit): ")
-print(tasks)
+print()
+
+while True:
+    print("""What would you like to do?
+        1. Remove item
+        2. View tasks
+        3. Q to quit""")
+
+    user_action = input("Enter a choice? ")
+    if user_action == "1":
+        user_removal = input("What would you like to remove? ")
+        tasks.remove(user_removal)
+        print(f"You removed {user_removal}")
+    elif user_action == "2":
+        print("---Your tasks---")
+        for task in tasks:
+            print(f"* {task}")
+    else:
+        user_action.lower()
+        print("Bye bye!")
+        break
